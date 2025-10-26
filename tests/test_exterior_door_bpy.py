6e58686d81a1be146f928e79ec4d1d81944741c9
jkim3191 <jkim3191@gatech.edu>
2025-10-26 19:38:28 -0400

    install and enable add-on

--- tests/test_exterior_door_bpy.py
+++ tests/test_exterior_door_bpy.py
@@ -2,17 +2,24 @@ import bpy
 import os
 from pathlib import Path
 
+# Install and enable add-on
+addon_path = "/tmp/Door It! Interior 4.0,4.1.zip"
+bpy.ops.preferences.addon_install(filepath=addon_path)
+print("Addon installed")
+
+# Enable the addon
+bpy.ops.preferences.addon_enable(module="DoorItInterior")
+bpy.ops.wm.save_userpref()
+print("Addon enabled and preferences saved")
+
 # Get the directory of the current script
 script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
-# Path to the script to be executed
-script_to_run = (script_dir / "../bpy_script/Exterior_door_bpy.py").resolve()
-
-# Path for the output .blend file
-output_blend_file = script_dir / "output.blend"
+# script_to_run = (script_dir / "../bpy_script/randomizing_door.py").resolve()
 
 # Execute the script
-exec(compile(script_to_run.read_text(), str(script_to_run), 'exec'), {'__file__': str(script_to_run)})
+# exec(compile(script_to_run.read_text(), str(script_to_run), "exec"), {"__file__": str(script_to_run)})
+output_blend_file = script_dir / "addon_installed.blend"
 
 # Save the current scene to a new .blend file
 bpy.ops.wm.save_as_mainfile(filepath=str(output_blend_file))
